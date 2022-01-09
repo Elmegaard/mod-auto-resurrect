4afeeae70012b4e81ffc007b9206292b25165dd3
Elmegaard <elmegaard39@gmail.com>
2022-01-09 03:35:24 +0100

    Initial commit

--- src/mod_auto_resurrect.cpp
+++ src/mod_auto_resurrect.cpp
@@ -7,7 +7,7 @@
 #include "Config.h"
 #include "Chat.h"
 
-// Add player scripts
+ // Add player scripts
 class AutoResurrect : public PlayerScript
 {
 public:
@@ -15,9 +15,31 @@ public:
 
     void OnLogin(Player* player) override
     {
-        if (sConfigMgr->GetOption<bool>("MyModule.Enable", false))
+        if (sConfigMgr->GetOption<bool>("AutoResurrect.Enable", false) && sConfigMgr->GetOption<bool>("AutoResurrect.Notification", false))
         {
-            ChatHandler(player->GetSession()).SendSysMessage("Hello World from Skeleton-Module!");
+            ChatHandler(player->GetSession()).SendSysMessage("Auto Resurrect module enabled");
+        }
+    }
+
+    void OnPlayerReleasedGhost(Player* player) override {
+        if (!sConfigMgr->GetOption<bool>("AutoResurrect.Enable", false))
+        {
+            return;
+        }
+
+        bool dungeons = sConfigMgr->GetOption<bool>("AutoResurrect.Dungeon", false);
+        bool heroics = sConfigMgr->GetOption<bool>("AutoResurrect.HeroicDungeon", false);
+        bool raids = sConfigMgr->GetOption<bool>("AutoResurrect.Raid", false);
+        bool heroicRaids = sConfigMgr->GetOption<bool>("AutoResurrect.HeroicRaid", false);
+
+        if ((dungeons && !player->GetMap()->IsHeroic() && player->GetMap()->IsDungeon()) ||
+            (heroics && player->GetMap()->IsHeroic() && player->GetMap()->IsDungeon()) ||
+            (raids && !player->GetMap()->IsHeroic() && player->GetMap()->IsRaid()) ||
+            (heroicRaids && player->GetMap()->IsHeroic() && player->GetMap()->IsRaid())) {
+            AreaTriggerTeleport const* at = sObjectMgr->GetMapEntranceTrigger(player->GetMapId());
+
+            player->ResurrectPlayer(1.0f);
+            player->TeleportTo(at->target_mapId, at->target_X, at->target_Y, at->target_Z, at->target_Orientation);
         }
     }
 };
